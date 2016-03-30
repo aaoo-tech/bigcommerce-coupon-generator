a06b6f5edf0854acfcffe62c5b0e4dfc0235e384
Allen Koo <crossgate9@gmail.com>
2016-03-30 12:30:29 +0800

    bug fix

--- api/services/CouponService.js
+++ api/services/CouponService.js
@@ -8,6 +8,12 @@
 var  _ = require('underscore');
     
 module.exports = {
+  errors: {
+    NUMBER_NOT_VALID: 'Number not valid.',
+    NAME_NOT_VALID: 'Name not valid.',
+    CHARSET_NOT_VALID: 'Charset not valid.',
+    DISCOUNT_TYPE_NOT_VALID: 'Discount type not valid',
+  },
   charsets: [
     { name: 'Number Only', chars: '0123456789' },
     { name: 'Lowercase Letter Only', chars: 'abcdefghijqlmnopqrstuvwxyz' },
@@ -27,9 +33,41 @@ module.exports = {
     });
   },
   generate: function(params, old_codes) {
+
+    if (params.number <= 0 || UtilityService.is_int(params.number) === false) {
+      throw new Error(this.errors.NUMBER_NOT_VALID);
+    }
+
+    if (_.isNull(params.coupon_name) === true || 
+        _.isUndefined(params.coupon_name) === true ||
+        params.coupon_name === '') {
+      throw new Error(this.errors.NAME_NOT_VALID);
+    }
+
+    if (params._charset < 0 || params._charset >= this.charsets.length ||
+        _.isNull(params._charset) === true || _.isUndefined(params._charset) === true) {
+      throw new Error(this.errors.CHARSET_NOT_VALID);
+    }
+
+    if (params.discount_type < 0 || params.discount_type >= this.discount_types.length ||
+        _.isNull(params.discount_type) === true || _.isUndefined(params.discount_type) === true) {
+      throw new Error(this.errors.DISCOUNT_TYPE_NOT_VALID);
+    }
+
+    if (_.isNull(params._prefix) === true ||
+        _.isUndefined(params._prefix) === true) {
+      params._prefix = '';
+    }
+
+    if (_.isNull(params.suffix) === true || 
+        _.isUndefined(params.suffix) === true) {
+      params.suffix = '';
+    }
+
     _charset = this.charsets[params._charset].chars;
 
     // generate coupons
+    // [TODO] improve performance
     var data = [];
     for (var i = 0; i < parseInt(params.number); i++) {
         var coupon_code = {};
@@ -50,8 +88,7 @@ module.exports = {
                 'expire_date': params.expire_date
             };
         } while (_.where(data, {code: coupon_code.code}).length > 0 || _.where(old_codes, {code: coupon_code.code}).length > 0);
-        // while (_.where(data, coupon_code).length > 0 || _.where(old_codes, coupon_code).length > 0);
-
+        
         data.push(coupon_code);
     }
 
--- api/services/UtilityService.js
+++ api/services/UtilityService.js
@@ -1,5 +1,48 @@
+var _ = require('underscore');
+
 module.exports = {
+
+  clone: function(obj) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = this.clone.call(this, obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) {
+              copy[attr] = this.clone.call(this, obj[attr]);
+            }
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+  },
+
   is_int: function(n) {
+    if (_.isArray(n) === true) {
+      return false;
+    }
+    
     return n % 1 === 0;
   },
 
