9354ea32e35058dc4bec88b90995f64f11758c35
Allen Koo <crossgate9@gmail.com>
2016-03-30 15:40:15 +0800

    [add] cron job process script

--- crontab/process.js
+++ crontab/process.js
@@ -9,147 +9,192 @@ module.exports = {
     async.waterfall([
       // check whether cron job is running
       function(cb) { 
-        Setting.find({field: 'cron_running'}).exec(function(err, settings) {
-         // console.log(settings);
-          if (settings[0].value == '1') {
-            cb(null, false);
-          } else {
-            Setting.update(
-              {id: settings[0].id}, 
-              {value: '1'}
-            ).exec(function(err) {
-              // console.log(err);
-              cb(null, true);
-            });
-          }
+        SettingService.get('cron_running', function(err, setting) {
+          cb(
+            (setting.value != '0') ? 
+              'Cron job is running' : null
+          );
+        });
+      },
+      // update cron job status
+      function(cb) {
+        SettingService.set('cron_running', '1', function() {
+          cb(null);
         });
       },
       // fetch the task
-      function(_can_run, cb) {
-        if (_can_run === false) {
-          cb(null, false);
-          return;
-        }
-
+      function(cb) {
         Tasks.findOne({
           where: { status: 1 },
           sort: 'id ASC'
         }).exec(function(err, task) {
           if (err) {
+            cb(err);
           } else {
-            Tasks.update({
-              id: task.id
-            }, {
-              status: 2
-            }).then(function() {
-              cb(null, _can_run, task);
-            });
+            task._rules = JSON.pare(task._rules);
+            cb(null, task);
           }
         }
       },
-      // fetch the website
-      function(_can_run, task, cb) {
-        if (_can_run === false) {
-          cb(null, false);
-          return;
-        }
-
-        Website.findOne({
-          // task_id: task.id
-          url: task.url
-        }).exec(function(err, website) {
-          if (err) {
-
-          } else {
-            cb(null, _can_run, task, website);
-          }
-        });
-      },
-      // run the task
-      function(_can_run, task, website, cb) {
-        if (_can_run === false) {
-          cb(null, false);
-          return;
-        }
-
-        var website_codes = JSON.parse(website.coupon);
-        website_codes = website_codes.map(function(entry) {
-          return entry.code;
-        });
-
-        CsvService._read(task.csv_filename, function(csvs) {
-          var csv_codes = csvs.map(function(entry) {
-            return entry.code;
-          });
-
-          var duplicated_codes = _.intersect(website_codes, csv_codes);
-          var code_collection = _.union(website_codes, csv_codes);
-          var rules = task._rules;
-
-          // regenerate duplocated coupons
-          if (task.is_upload == 0 && duplicated_codes.length > 0) {
-            rules.number = duplicated_codes.length;
-
-            code_collection = _.union(
-              code_collection, 
-              CouponService.generate(rules, code_collection)
-            );
-          }
-
-          var task_name = rule.coupon_name,
-              task_discount_type = rule.discount_type,
-              task_discount_amount = rule.discount_amount,
-              task_max_uses = rule.max_uses,
-              task_num_uses = rule.num_uses;
-
-          if (task.is_upload == 1) {
-            task_name = csvs.name;
-            task_discount_type = csvs.discount_type;
-            task_discount_amount = csvs.discount_amount;
-            task_max_uses = csvs.max_uses;
-            task_num_uses = csvs.num_uses;
-          } 
-
-          async.eachSeries(code_collection, function(code, cb) {
-            BigCommerceService.create({
-              username: task.username,
-              host: task.url,
-              token: task.token 
-            }, {
-              'name': task_name,
-              'type': task_discount_type,
-              'code': _code.code,
-              'amount': task_discount_amount,
-              'enabled': true,
-              'applies_to': {
-                  'entity': 'categories',
-                  'ids': '[' + _task.category + ']'
-              },
-              'max_uses': task_max_uses,
-              'num_uses': task_num_uses
-            }, function(response) {
-              sleep(SLEEP_INTERVAL);
-              cb();
-            });
-          });
-        }, function done() {
+      // update the task status
+      function(task, cb) {
+        Tasks.update({
+          id: task.id
+        }, {
+          status: 2
+        }).then(function() {
           cb(null, task);
         });
       },
-      // update task status
+      // fetch task related csv file
       function(task, cb) {
+        CsvService.read(task.csv_filename, function(err, csvs) {
+          if (err) {
+            cb(err);
+          } else {
+            task.coupon = csvs.map(function(entry) {
+              return entry.code;
+            });
+
+            cb(null, task, csvs);
+          }
+        });
+      },
+      // fetch the website
+      function(task, csvs, cb) {
+        Website.findOne({
+          url: task.url
+        }).exec(function(err, website) {
+          if (err) {
+            cb(err);
+          } else {
+            website.coupon = JSON.parse(website.coupon);
+            website.coupon = website.coupon.map(function(entry) {
+              return entry.code;
+            });
+            cb(null, task, website, csvs);
+          }
+        });
+      },
+      // prepare the task
+      function(task, website, csvs, cb) {
+        // check duplicate coupons
+        var duplicated_codes = _.intersect(website.coupon, task.coupon);
+        var code_collection = _.union(website.coupon, task.coupon);
+
+        if (duplicated_codes.length > 0) {
+          if (task.is_upload == 1) {
+            task.coupon = _.difference(task.coupon, duplicated_codes);
+          }
+
+          if (task.is_upload == 0) {
+            var rules = UtilityService.clone(task._rules);
+            rules.number = duplicated_codes.length;
+
+            task.coupon = _.union(
+              CouponService.generate(rules, code_collection),
+              _.difference(task.coupon, duplicated_codes)
+            );
+          }
+        }
+
+        var coupons = [];
+
+        if (task.is_upload == 0) {
+          _.each(task.coupon, function(coupon) {
+            var tmp = {};
+            tmp['name'] = task._rules.coupon_name;
+            tmp['type'] = task._rules.discount_type;
+            tmp['amount'] = task._rules.discount_amount;
+            tmp['code'] = coupon;
+            tmp['enable'] = true;
+            tmp['applies_to'] = {};
+
+            if (_.isNull(task._rules.max_uses) === false && 
+                _.isUndefined(task._rules.max_uses) === false) {
+              tmp['max_uses'] = task._rules.max_uses;
+            }
+
+            if (_.isNull(task._rules.num_uses) === false && 
+                _.isUndefined(task._rules.num_uses) === false) {
+              tmp['num_uses'] = task._rules.num_uses;
+            }
+
+            if (task._rules.category.length > 0) {
+              tmp['applies_to'] = {
+                'entity': 'categories',
+                'ids': task._rules.category.split(',')
+              };
+            }
+
+            coupons.push(tmp);
+          });
+        } else {
+          _.each(csvs, function(entry) {
+            if (task.coupon.indexOf(entry.code) == -1) {
+              return;
+            }
+
+            var tmp = {};
+            tmp['name'] = entry.name;
+            tmp['type'] = entry.discount_type;
+            tmp['amount'] = entry.discount_amount;
+            tmp['code'] = entry.code;
+            tmp['enable'] = true;
+            tmp['applies_to'] = {};
+
+            if (_.isNull(entry.max_uses) === false && 
+                _.isUndefined(entry.max_uses) === false) {
+              tmp['max_uses'] = entry.max_uses;
+            }
+
+            if (_.isNull(entry.num_uses) === false && 
+                _.isUndefined(entry.num_uses) === false) {
+              tmp['num_uses'] = entry.num_uses;
+            }
+
+            // if (entry.category.length > 0) {
+            //   tmp['applies_to'] = {
+            //     'entity': 'categories',
+            //     'ids': entry.category.split(',')
+            //   };
+            // }
+
+            coupons.push(tmp);
+          });
+        }
+
+        cb(null, task, website, csvs, coupons);
+      },
+      // run the task
+      // function(task, website, csvs, coupons, cb) {
+      //   async.eachSeries(coupons, function(coupon, cb) {
+      //     BigCommerceService.create({
+      //       username: task.username,
+      //       host: task.url,
+      //       token: task.token 
+      //     }, coupon, function(response) {
+      //       sleep(SLEEP_INTERVAL);
+      //       cb();
+      //     });
+      //   }, function() {
+      //     cb(null, task, website, csvs, coupons);
+      //   });
+      // },
+      // update task status
+      function(task, website, csvs, coupons, cb) {
         Tasks.update({
           id: task.id
         },{
           status: 3
         }).then(function(updated) {
-          cb(null, task);
+          cb(null, task, website, csvs, coupons);
         }).catch(function(err) {
           cb(err);
         });
       },
       // send email
-      function(task, cb) {
+      function(task, website, csvs, coupons, cb) {
         EmailService.send(
           sails.config.email.template.complete,
           {
@@ -159,10 +204,14 @@ module.exports = {
             'task': task
           },
           function(err, info) {
-            cb(null, task);
+            cb(err, task);
           }
         );
       }
-    ]);
+    ], function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 };
